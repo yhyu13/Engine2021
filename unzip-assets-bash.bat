3f7ab1ccad8ff7bcf58503017647d9c2c641a943
yhyu13 <yohan680919@gmail.com>
2023-06-25 20:36:00 +0800

    Use tar instead of unzip as Win10 natively has tar but unzip requires GNU packages

--- unzip-assets-bash.bat
+++ unzip-assets-bash.bat
@@ -1,14 +1,14 @@
 :: unzip engine content
 cd ./engine/
-call unzip -q -o asset.zip 
-call unzip -q -o vendors.zip 
+call tar -xf asset.zip 
+call tar -xf vendors.zip 
 cd ..
 
 :: unzip sample project content
 cd ./samples/demo/application
-call unzip -q -o asset.zip 
+call tar -xf asset.zip 
 cd ../../..
 
 cd ./samples/asteriod/application
-call unzip -q -o asset.zip 
+call tar -xf asset.zip 
 cd ../../..
